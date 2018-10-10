54f62c10dc26d9684963aa2c323566b977754070
jetoile <kmx.petals@gmail.Com>
2018-10-10 18:12:33 +0200

    allow hadoop-unit-standalone to use maven conf if found

--- hadoop-unit-standalone/src/main/java/fr/jetoile/hadoopunit/HadoopStandaloneBootstrap.java
+++ hadoop-unit-standalone/src/main/java/fr/jetoile/hadoopunit/HadoopStandaloneBootstrap.java
@@ -13,15 +13,24 @@
  */
 package fr.jetoile.hadoopunit;
 
+import com.google.inject.AbstractModule;
 import fr.jetoile.hadoopunit.exception.BootstrapException;
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.maven.cli.logging.Slf4jLoggerManager;
 import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
+import org.apache.maven.settings.Settings;
+import org.apache.maven.settings.building.DefaultSettingsBuildingRequest;
+import org.apache.maven.settings.building.SettingsBuilder;
+import org.apache.maven.settings.building.SettingsBuildingException;
+import org.apache.maven.settings.building.SettingsBuildingRequest;
+import org.codehaus.plexus.*;
+import org.codehaus.plexus.classworlds.ClassWorld;
+import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
 import org.eclipse.aether.DefaultRepositorySystemSession;
 import org.eclipse.aether.RepositorySystem;
-import org.eclipse.aether.RepositorySystemSession;
 import org.eclipse.aether.artifact.Artifact;
 import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.collection.CollectRequest;
@@ -40,7 +49,7 @@ import org.eclipse.aether.transport.file.FileTransporterFactory;
 import org.eclipse.aether.transport.http.HttpTransporterFactory;
 import org.eclipse.aether.util.artifact.JavaScopes;
 import org.eclipse.aether.util.filter.DependencyFilterUtils;
-import org.fusesource.jansi.AnsiConsole;
+import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -54,6 +63,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static fr.jetoile.hadoopunit.HadoopUnitConfig.DEFAULT_PROPS_FILE;
 import static org.fusesource.jansi.Ansi.Color.GREEN;
 
 public class HadoopStandaloneBootstrap {
@@ -65,6 +75,43 @@ public class HadoopStandaloneBootstrap {
     static private List<ComponentProperties> componentsToStop = new ArrayList<>();
     static private List<ComponentProperties> componentsProperty = new ArrayList();
 
+    /**
+     * code from org.apache.maven.cli.MavenCli.container(CliRequest)
+     * <p>
+     * cf also https://github.com/igor-suhorukov/mvn-classloader/blob/master/dropship/src/main/java/com/github/smreed/dropship/ClassLoaderBuilder.java
+     */
+    private static PlexusContainer mvnContainer() {
+        ILoggerFactory slf4jLoggerFactory = LoggerFactory.getILoggerFactory();
+        Slf4jLoggerManager plexusLoggerManager = new Slf4jLoggerManager();
+
+        ClassWorld classWorld = new ClassWorld("plexus.core", Thread.currentThread().getContextClassLoader());
+
+        DefaultPlexusContainer container = null;
+
+        ContainerConfiguration cc = new DefaultContainerConfiguration()
+                .setClassWorld(classWorld)
+                .setClassPathScanning(PlexusConstants.SCANNING_INDEX)
+                .setAutoWiring(true)
+                .setName("maven");
+
+        try {
+            container = new DefaultPlexusContainer(cc, new AbstractModule() {
+                protected void configure() {
+                    bind(ILoggerFactory.class).toInstance(slf4jLoggerFactory);
+                }
+            });
+        } catch (PlexusContainerException e) {
+            LOGGER.error("unable to create PlexusContainer", e);
+        }
+
+        // NOTE: To avoid inconsistencies, we'll use the TCCL exclusively for lookups
+        container.setLookupRealm(null);
+
+        container.setLoggerManager(plexusLoggerManager);
+        Thread.currentThread().setContextClassLoader(container.getContainerRealm());
+
+        return container;
+    }
 
     public static RepositorySystem newRepositorySystem() {
         DefaultServiceLocator locator = MavenRepositorySystemUtils.newServiceLocator();
@@ -84,8 +131,54 @@ public class HadoopStandaloneBootstrap {
 
     public static DefaultRepositorySystemSession newRepositorySystemSession(RepositorySystem system) {
         DefaultRepositorySystemSession session = MavenRepositorySystemUtils.newSession();
+        String localRepositoryDir = "";
 
-        LocalRepository localRepo = new LocalRepository(hadoopUnitConfiguration.getString("maven.local.repo"));
+
+        String mavenHome = getInstalledMavenHome();
+
+        if (StringUtils.isNotEmpty(mavenHome)) {
+            //if MAVEN_HOME or M2_HOME are defined, will read maven's configuration throught settings.xml
+            LOGGER.info("is going to use the local maven configuration: {}", mavenHome);
+            File globalSettingsFile = new File(mavenHome + "/conf/settings.xml");
+            if (!globalSettingsFile.exists()) {
+                LOGGER.error("maven global settings.xml file not found : " + globalSettingsFile);
+            }
+
+            File userSettingsFile = new File(System.getProperty("user.home") + "/.m2/settings.xml");
+            if (!userSettingsFile.exists()) {
+                LOGGER.info("maven user settings.xml override file not found : " + userSettingsFile);
+            }
+
+            PlexusContainer container = mvnContainer();
+
+            SettingsBuilder defaultSettingsBuilder = null;
+            try {
+                defaultSettingsBuilder = container.lookup(SettingsBuilder.class);
+            } catch (ComponentLookupException e) {
+                LOGGER.error("unable to lookup SettingsBuilder", e);
+            }
+            SettingsBuildingRequest settingsRequest = new DefaultSettingsBuildingRequest();
+            settingsRequest.setGlobalSettingsFile(globalSettingsFile);
+            settingsRequest.setUserSettingsFile(userSettingsFile);
+            Settings settings = null;
+            try {
+                settings = defaultSettingsBuilder.build(settingsRequest).getEffectiveSettings();
+            } catch (SettingsBuildingException e) {
+                LOGGER.error("unable to get settings", e);
+            }
+
+            localRepositoryDir = settings.getLocalRepository();
+            if (localRepositoryDir == null) {
+                LOGGER.debug("is going to use default maven local repository");
+                localRepositoryDir = System.getProperty("user.home") + "/.m2/repository";
+            }
+            LOGGER.debug("is going to use {} repository" + localRepositoryDir);
+        } else {
+            LOGGER.debug("is going to use the maven repository from {} with key {}", DEFAULT_PROPS_FILE, "maven.central.repo");
+            localRepositoryDir = hadoopUnitConfiguration.getString("maven.local.repo");
+        }
+
+        LocalRepository localRepo = new LocalRepository(localRepositoryDir);
         session.setLocalRepositoryManager(system.newLocalRepositoryManager(session, localRepo));
 
         boolean debug = Boolean.parseBoolean(hadoopUnitConfiguration.getString("maven.debug"));
@@ -97,7 +190,32 @@ public class HadoopStandaloneBootstrap {
         return session;
     }
 
-    public static List<RemoteRepository> newRepositories(RepositorySystem system, RepositorySystemSession session) {
+    private static String getInstalledMavenHome() {
+        String maven_home = System.getenv("MAVEN_HOME");
+        String m2_home = System.getenv("M2_HOME");
+
+        String mavenHome = "";
+        if (StringUtils.isNotEmpty(maven_home)) {
+            LOGGER.debug("is going to use MAVEN_HOME to read configuration");
+            mavenHome = maven_home;
+        }
+        if (StringUtils.isNotEmpty(m2_home)) {
+            LOGGER.debug("is going to use M2_HOME to read configuration");
+            mavenHome = m2_home;
+        }
+        return mavenHome;
+    }
+
+    private static RepositorySystem getRepositorySystem() throws ComponentLookupException {
+        if (StringUtils.isNotEmpty(getInstalledMavenHome())) {
+            PlexusContainer mvnContainer = mvnContainer();
+            return mvnContainer.lookup(RepositorySystem.class);
+        } else {
+            return newRepositorySystem();
+        }
+    }
+
+    public static List<RemoteRepository> newRepositories() {
         return new ArrayList<>(Arrays.asList(newCentralRepository()));
     }
 
@@ -105,7 +223,7 @@ public class HadoopStandaloneBootstrap {
         return new RemoteRepository.Builder("central", "default", hadoopUnitConfiguration.getString("maven.central.repo")).build();
     }
 
-    public static void main(String[] args) throws BootstrapException, MalformedURLException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, DependencyResolutionException {
+    public static void main(String[] args) throws BootstrapException {
         String homeDirectory = ".";
         if (StringUtils.isNotEmpty(System.getenv("HADOOP_UNIT_HOME"))) {
             homeDirectory = System.getenv("HADOOP_UNIT_HOME");
@@ -115,7 +233,7 @@ public class HadoopStandaloneBootstrap {
 
         try {
             configuration = new PropertiesConfiguration("hadoop.properties");
-            hadoopUnitConfiguration = new PropertiesConfiguration("hadoop-unit-default.properties");
+            hadoopUnitConfiguration = new PropertiesConfiguration(DEFAULT_PROPS_FILE);
         } catch (ConfigurationException e) {
             throw new BootstrapException("bad config", e);
         }
@@ -126,21 +244,30 @@ public class HadoopStandaloneBootstrap {
             }
         });
 
-        RepositorySystem system = newRepositorySystem();
+        RepositorySystem system = null;
+        try {
+            system = getRepositorySystem();
+        } catch (ComponentLookupException e) {
+            LOGGER.error("unable to get RepositoySystem from external maven", e);
+        }
         DefaultRepositorySystemSession session = newRepositorySystemSession(system);
         DependencyFilter classpathFilter = DependencyFilterUtils.classpathFilter(JavaScopes.RUNTIME);
 
+        RepositorySystem finalSystem = system;
         componentsToStart.stream().forEach(c -> {
             Artifact artifact = new DefaultArtifact(hadoopUnitConfiguration.getString(c.getArtifactKey()));
             CollectRequest collectRequest = new CollectRequest();
             collectRequest.setRoot(new Dependency(artifact, JavaScopes.RUNTIME));
-            collectRequest.setRepositories(newRepositories(system, session));
+
+            if (StringUtils.isNotEmpty(getInstalledMavenHome())) {
+                collectRequest.setRepositories(newRepositories());
+            }
 
             DependencyRequest dependencyRequest = new DependencyRequest(collectRequest, classpathFilter);
 
             List<ArtifactResult> artifactResults = null;
             try {
-                artifactResults = system.resolveDependencies(session, dependencyRequest).getArtifactResults();
+                artifactResults = finalSystem.resolveDependencies(session, dependencyRequest).getArtifactResults();
             } catch (DependencyResolutionException e) {
                 e.printStackTrace();
             }
@@ -232,7 +359,7 @@ public class HadoopStandaloneBootstrap {
         try {
             Thread.currentThread().setContextClassLoader(classloader);
 
-            Object o = mainClass.getConstructor(URL.class).newInstance(HadoopStandaloneBootstrap.class.getClassLoader().getResource(HadoopUnitConfig.DEFAULT_PROPS_FILE));
+            Object o = mainClass.getConstructor(URL.class).newInstance(HadoopStandaloneBootstrap.class.getClassLoader().getResource(DEFAULT_PROPS_FILE));
             main = mainClass.getMethod("start");
             main.invoke(o);
             return new ComponentProperties(o, mainClass);
